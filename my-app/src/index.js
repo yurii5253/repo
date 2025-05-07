bc6f21dad448ae0d1976c567f2e882a55547d134
yurii5253 <romanenkoyurii6@gmail.com>
2025-05-07 10:26:09 +0300

    Lol

---
+++ my-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h2>Hello</h2>
+    <h2>Romanenko</h2>
+  </React.StrictMode>
+);
+
+
